fc37eeb6297bd7b4cd82f8d2ca3a43cc8cb74ffe
sshomaaa <ss0513.t@gmail.com>
2020-09-19 07:34:50 +0900

    fix

--- s_estate.go
+++ s_estate.go
@@ -36,8 +36,7 @@ func getEstateDetail(c echo.Context) error {
 
 	var estate *Estate
 	if estate, ok := cacheEstate[int64(id)]; !ok {
-		c.Echo().Logger.Infof("cache not found, get=%v", id)
-		err = dbe.Get(&estate, "SELECT * FROM estate WHERE id = ?", id)
+		err = dbe.Get(estate, "SELECT * FROM estate WHERE id = ?", id)
 		if err != nil {
 			if err == sql.ErrNoRows {
 				c.Echo().Logger.Infof("getEstateDetail estate id %v not found", id)
